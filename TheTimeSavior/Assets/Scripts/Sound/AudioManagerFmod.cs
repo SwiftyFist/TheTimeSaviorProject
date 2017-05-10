ff9e3d0b6486306d730612e4ca8fcae720edc577
Stefano Rovida <caneslotocato1@gmail.com>
2017-05-10 14:13:42 +0200

    Implementazione fmod dentro al progetto e start dell'audiomanagerfmod

---
+++ TheTimeSavior/Assets/Scripts/Sound/AudioManagerFmod.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD;
+
+public class AudioManagerFmod : MonoBehaviour {
+
+    [HideInInspector]
+    public List<EnemyAI> normalEnemyList;
+    [HideInInspector]
+    public List<DroneAI_v2> droneList;
+    [HideInInspector]
+    public Transform player;
+
+    [EventRef]
+    public string enemyBank;
+    [EventRef]
+    public string droneBank;
+    [EventRef]
+    public string playerShoot;
+    [EventRef]
+    public string playerMove;
+
+
+    private FMOD.Studio.EventInstance enemyInstance;
+    private FMOD.Studio.EventInstance droneInstance;
+    private FMOD.Studio.EventInstance playerInstance;
+
+
+    private void Start()
+    {
+        enemyInstance = FMODUnity.RuntimeManager.CreateInstance(enemyBank);
+        droneInstance = FMODUnity.RuntimeManager.CreateInstance(droneBank);
+        
+    }
+
+
+    public void EnemySound(Transform enemy, float value)
+    {
+        enemyInstance.set3DAttributes(RuntimeUtils.To3DAttributes(enemy));
+        enemyInstance.setParameterValue("value", value);
+    }
+
+
+    public void DroneSound(Transform drone, float value)
+    {
+        droneInstance.set3DAttributes(RuntimeUtils.To3DAttributes(drone));
+        droneInstance.setParameterValue("value", value);
+
+    }
+
+    private void Stop()
+    {
+        FMOD.Studio.Bus droneBus = RuntimeManager.GetBus("bus:/drone");
+        FMOD.Studio.Bus enemyBus = RuntimeManager.GetBus("bus:/enemy");
+        droneBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        enemyBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        enemyInstance.release();
+        droneInstance.release();
+    }
+
+    private void OnDestroy()
+    {
+        Stop();
+    }
+
+
+}
