5dac7c036f3e1aea8cd5121c48c34f3d478f061d
Stefano Rovida <caneslotocato1@gmail.com>
2017-05-16 21:08:45 +0200

    Main menu iniziata e aggiunte sprites varie

---
+++ TheTimeSavior/Assets/Scripts/GUI/MainMenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+    [SerializeField]
+    private float timeToWait;
+    [SerializeField]
+    private float sizeMoltiplier;
+    [SerializeField]
+    private GameObject[] menuSprites;
+
+
+    private int counter = 0;
+
+
+    private IEnumerator LerpCoroutine(Vector2 startScale, Vector2 endScale, float t)
+    {
+        float time = 0;
+        float addingTime = Time.deltaTime;
+
+        while (time < t)
+        {
+            menuSprites[counter].transform.localScale = Vector2.Lerp(startScale, endScale, (time / t));
+            time += addingTime;
+            addingTime += 0.001f;
+            yield return null;
+        }
+
+        if (time >= t)
+        {
+            menuSprites[counter].SetActive(false);
+            counter++;
+            menuSprites[counter].SetActive(true);
+        }
+    }
+
+}
