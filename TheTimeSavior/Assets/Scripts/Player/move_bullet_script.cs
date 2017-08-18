7478cc2aaa6368f39bf0ef286bce712bed934aa1
Davide.Plessi <d.plessi@nsi.it>
2017-08-18 11:36:19 +0200

    Ci piace il refactoring

--- TheTimeSavior/Assets/Scripts/Player/move_bullet_script.cs
+++ TheTimeSavior/Assets/Scripts/Player/move_bullet_script.cs
@@ -2,35 +2,19 @@
 using System.Collections;
 using Enemies;
 
-public class move_bullet_script : MonoBehaviour {
-
-	//Velocità del proiettile 
-	public int moveSpeed ;
-	public int pointsToAdd ;
-	public int damageToGive;
-
-
-	//Effetto Colpo Scudo
-	Transform SpawnEffect;
-	public Transform EffectPrefab;
-
-	//Camera Shake
-	public float EnemyShakeAmt ;
-	public float EnemyShakeLenght ;
-
-	Vector3 bulletDir;
-
-	void Awake()
-	{
-		SpawnEffect = transform.Find("Spawn_Effect");
-	}
+public class move_bullet_script : MonoBehaviour
+{
+	public int MoveSpeed = 55;
+	public int PointsToAdd = 10;
+	public int DamageToGive =1;
+	public float EnemyShakeAmt;
+	public float EnemyShakeLenght;
 
 	void Update () 
 	{
 		if (GetComponent<Transform>().position.x > Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x)
 			Destroy(gameObject);
-		bulletDir = this.gameObject.transform.forward;
-		transform.Translate (Vector3.right * Time.deltaTime * moveSpeed);
+		transform.Translate (Vector3.right * Time.deltaTime * MoveSpeed);
 		Destroy (gameObject, 0.5f);
 	}
 
@@ -44,14 +28,12 @@ public class move_bullet_script : MonoBehaviour {
             GameObject.Find("Camera").GetComponent<Camera_Shake_Script>().Shake(EnemyShakeAmt, EnemyShakeLenght);
 
             colInfo.GetComponent<EnemySoundManager>().PlayOnHitByBullet();
-            colInfo.GetComponent<enemy_health_manager_script>().giveDamage(damageToGive);
+            colInfo.GetComponent<enemy_health_manager_script>().giveDamage(DamageToGive);
             colInfo.GetComponent<Enemy>().SetTrigger();
         }
 
 		if (colInfo.tag == "LevelObject")
-        {
 			Destroy (gameObject);
-		}
 	}
 }
 
