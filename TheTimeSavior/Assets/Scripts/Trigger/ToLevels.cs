35bc331b4beb635e1bd4af0456eece1e47ad99a7
SwiftyFist <liquidc93@gmail.com>
2017-09-27 20:42:36 +0200

    Entrata nel level test

---
+++ TheTimeSavior/Assets/Scripts/Trigger/ToLevels.cs
@@ -0,0 +1,28 @@
+﻿using GameManager;
+using UnityEngine;
+
+namespace Trigger
+{
+    public class ToLevels : MonoBehaviour
+    {
+        private bool _canGo;
+        
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!collision.gameObject.CompareTag("Player")) return;
+            _canGo = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (!collision.gameObject.CompareTag("Player")) return;
+            _canGo = false;
+        }
+
+        private void Update()
+        {
+            if (_canGo && Input.GetButtonDown("Submit"))
+                LevelManager.GoToTest();
+        }
+    }
+}
